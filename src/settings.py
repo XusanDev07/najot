7b33dd1fb47c5de29999984a09c133ab09adac09
Joha-JST <133357417+XusanDev07@users.noreply.github.com>
2023-08-04 18:54:57 +0500

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -153,6 +153,8 @@ if not DEBUG:
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'static/root'
+
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
