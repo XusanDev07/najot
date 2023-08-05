94d8d0a3dc35fddbf2acf0c14e7d0551e54c770a
Joha-JST <132278335+Joha-JST@users.noreply.github.com>
2023-08-05 11:13:45 +0500

    Update xizmatlar.py

--- najot/models/xizmatlar.py
+++ najot/models/xizmatlar.py
@@ -3,4 +3,7 @@ from django.db import models
 
 class Xizmatlar(models.Model):
     name = models.CharField(max_length=128)
-    img = models.FileField(),
+    img = models.FileField()
+
+    def __str__(self):
+        return self.name
