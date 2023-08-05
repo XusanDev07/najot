4862ae5dda8ddc62ecb95c72a0b309d20fd27f98
Joha-JST <132278335+Joha-JST@users.noreply.github.com>
2023-08-05 11:14:59 +0500

    Update admin.py

--- najot/admin.py
+++ najot/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from najot.models.docktor_qushish import Doktor, Price, Position, DocTime, Professions, Clink
+from najot.models.docktor_qushish import Doktor, Price, Position, DocTime, Professions, Clink, Xizmatlar
 
 
 admin.site.register(Doktor)
@@ -8,3 +8,4 @@ admin.site.register(Position)
 admin.site.register(Professions)
 admin.site.register(DocTime)
 admin.site.register(Clink)
+admin.site.register(Xizmatlar)
