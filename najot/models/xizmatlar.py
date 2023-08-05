93942677b54abf55e5cc59df4884bbbfc7eb55fb
Joha-JST <132278335+Joha-JST@users.noreply.github.com>
2023-08-05 11:39:05 +0500

    Update xizmatlar.py

--- najot/models/xizmatlar.py
+++ najot/models/xizmatlar.py
@@ -5,5 +5,10 @@ class Xizmatlar(models.Model):
     name = models.CharField(max_length=128)
     img = models.FileField()
 
+    class Meta:
+        verbose_name = "Xizmat"
+        verbose_name_plural = "8. Xizmatlar"
+    
     def __str__(self):
         return self.name
+
