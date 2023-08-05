d04d6e1110cbb16742cd08bb955f8832efb90404
Joha-JST <132278335+Joha-JST@users.noreply.github.com>
2023-08-05 11:24:02 +0500

    Update docktor_qushish.py

--- najot/models/docktor_qushish.py
+++ najot/models/docktor_qushish.py
@@ -5,17 +5,35 @@ from najot.models import User
 class Professions(models.Model):
     name = models.CharField(max_length=256)
 
+    class Meta:
+    verbose_name = "Yonalishi"
+    verbose_name_plural = "3. Yonalishlar"
+    
+    def __str__(self):
+        return self.name
+
 
 class Clink(models.Model):
     name = models.CharField(max_length=128)
     info = models.TextField()
     img = models.ImageField("Clinka rasmi", upload_to="clink", null=True, blank=True)
 
+    class Meta:
+    verbose_name = "Clinika"
+    verbose_name_plural = "2. Clinikalar"
+    
     def __str__(self):
         return self.name
 
 class Position(models.Model):
-    name = models.CharField(max_length=128)
+    name = models.CharField("Ism", max_length=128)
+
+    class Meta:
+    verbose_name = "Lavozm"
+    verbose_name_plural = "4. Lavozmlar"
+    
+    def __str__(self):
+        return self.name
 
 
 class Doktor(models.Model):
@@ -44,31 +62,44 @@ class DocTime(models.Model):
     doc = models.ForeignKey(Doktor, on_delete=models.CASCADE)
     free = models.BooleanField(default=True)
 
+    class Meta:
+    verbose_name = "Doktor Time"
+    verbose_name_plural = "5. Doktorlar Vaqti"
+    
+    def __str__(self):
+        return f'{self.doc} || {self.free}'
+
 
 class Service(models.Model):
     name = models.CharField(max_length=128)
     info = models.TextField()
     icon = models.ImageField(upload_to="service")
 
-
-class DocReating(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="Reating")
-    doc = models.ForeignKey(Doktor, on_delete=models.CASCADE)
-    star = models.SmallIntegerField(choices=[
-        (1, "     ⭐️ "),
-        (2, "    ⭐️⭐️ "),
-        (3, "   ⭐️⭐️⭐️ "),
-        (4, "  ⭐️⭐️⭐️⭐️ "),
-        (5, " ⭐️⭐️⭐️⭐️⭐️ ")
-    ])
-
-    feed = models.TextField()
+    class Meta:
+    verbose_name = "Service"
+    verbose_name_plural = "6. Servislar"
+    
+    def __str__(self):
+        return self.name
 
 
-class Price(models.Model):
-    doc = models.ForeignKey(Doktor, on_delete=models.CASCADE)
-    service = models.ForeignKey(Service, on_delete=models.CASCADE)
-    icon = models.ImageField(upload_to="service")
+# class DocReating(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="Reating")
+#     doc = models.ForeignKey(Doktor, on_delete=models.CASCADE)
+#     star = models.SmallIntegerField(choices=[
+#         (1, "     ⭐️ "),
+#         (2, "    ⭐️⭐️ "),
+#         (3, "   ⭐️⭐️⭐️ "),
+#         (4, "  ⭐️⭐️⭐️⭐️ "),
+#         (5, " ⭐️⭐️⭐️⭐️⭐️ ")
+#     ])
+#     feed = models.TextField()
+
+
+# class Price(models.Model):
+#     doc = models.ForeignKey(Doktor, on_delete=models.CASCADE)
+#     service = models.ForeignKey(Service, on_delete=models.CASCADE)
+#     icon = models.ImageField(upload_to="service")
 
 
 class DocReating(models.Model):
@@ -98,3 +129,7 @@ class Price(models.Model):
             pr = pr.lower().replace(i, "")
         self.pr = int(pr)
         return super(Price, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.doc} || {self.service} || {self.price}'
+    
