50aaa4e665ae5e7cf02a0e8736c6242cfeba242d
Joha-JST <132278335+Joha-JST@users.noreply.github.com>
2023-08-11 18:06:18 +0500

    Update services.py

--- najot/methods/all_function/services.py
+++ najot/methods/all_function/services.py
@@ -11,3 +11,10 @@ def services_id(request, params):
             "Doktrning_ismi": services.doktor.name,
 
         }
+
+
+def all_services(request, params):
+    return {
+        "result": [i.services_format() for i in Contact.objects.all()]
+    }
+    
